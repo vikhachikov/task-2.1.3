3b70c19af337d8fca49bc6dd9e47ad5e5796ef04
Vik-Kan <vitek102001@gmail.com>
2023-11-05 20:38:48 +0300

    test0.2

--- src/main/java/app/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package app.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "app")
-public class AppConfig {
-}
